e6b53d8a72ef930e7d3c8064ac43d60ac36a85fc
poucheuflorian <poucheumbondaflorianwilfried@gmail.com>
2024-04-05 06:12:01 -0700

    continous

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 
+import uuid
+from datetime import datetime
+
 class BaseModel():
     """ model of all basics classes"""
+
     def __init__(self):
-        self.id = 1
-        self.created_at = 1
-        self.updated_at = 1
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
     
     def __str__(self):
-        return '[<{BaseModel}>] (<self.id>) <self.__dict__>'
+        return '[{}] ({}) {}'.format(BaseModel.__name__,self.id,self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        dict_ = self.__dict__
+        dict_[created_at] = dict_[created_at].isoformat()
+        dict_[updated_at] = dict_[created_at].isoformat()
+        dict_['__class__'] = BaseModel.__name__
