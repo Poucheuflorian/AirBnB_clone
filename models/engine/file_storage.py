9000b9fda772b340d3470ee64783bcd5d3526e81
poucheuflorian <poucheumbondaflorianwilfried@gmail.com>
2024-04-09 14:08:35 -0700

    first commit

---
+++ models/engine/file_storage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import json
+
+
+class FileStorage():
+    """ Storage of my program """
+
+    __file_path = "js.json"
+    __objects = {}
+
+    def all(self):
+        return self.__class__.__objects
+
+    def new(self, obj):
+        self.__class__.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        dict_save = {}
+        for key, value in self.__class__.__objects.items():
+            dict_save[key] = value.to_dict()
+        with open(self.__class__.__file_path, 'w', errors='ignore') as f:
+            json.dump(dict_save, f, sort_keys=True, indent=4)
+
